0033f5d2c633dca66b1544042292c4231e204b4c
Michael Westwood <michael@honcho.agency>
2026-01-29 15:22:11 +0000

    004: auto rewrite base url

--- src/config.php
+++ src/config.php
@@ -28,27 +28,28 @@ return [
 
     // An optional callback that defines whether to skip the transform logic.
     // The callback will be passed the Asset and the ImageTransform (or null if no transform is being applied).
-    // Return true to skip the transform logic and return the URL defined in the asset Filesystem.
+    // Return true to skip the transform logic and return the URL defined in the asset Filesystem. When this returns
+    // false, the plugin automatically rewrites the asset's base URL to use the configured Imgix domain.
     // Example: skip non-image assets when no transform is applied
-//    'skipTransform' => function(\craft\elements\Asset $asset, ?\craft\models\ImageTransform $transform = null) {
-//        return $asset->kind !== 'image' && $transform === null;
-//    },
+    //    'skipTransform' => function(\craft\elements\Asset $asset, ?\craft\models\ImageTransform $transform = null) {
+    //        return $asset->kind !== 'image' && $transform === null;
+    //    },
 
     // Default imgix parameters that will be applied to all images unless overridden.
-//    'imgixDefaultParams' => [
-//        'auto' => 'format,compress',
-//        'cs' => 'srgb',
-//    ],
+    //    'imgixDefaultParams' => [
+    //        'auto' => 'format,compress',
+    //        'cs' => 'srgb',
+    //    ],
 
-      // Override settings for each volume
-// 'volumes' => [
-//     'volume handle goes here' => [
-//          'devMode' => '',
-//          'enabled' => '',
-//          'imgixDomain' => '',
-//          'imgixDefaultParams' => [],
-//     ],
-// ]
+    // Override settings for each volume
+    // 'volumes' => [
+    //     'volume handle goes here' => [
+    //          'devMode' => '',
+    //          'enabled' => '',
+    //          'imgixDomain' => '',
+    //          'imgixDefaultParams' => [],
+    //     ],
+    // ]
 
 
 ];
